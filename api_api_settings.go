ef3962eccdac2ba202728dabba6fd8e64e0cdfa9
Aamir Parekh <aamir.parekh36@gmail.com>
2024-07-22 16:58:05 +0500

    Fixed createAPIKey method

--- api_api_settings.go
+++ api_api_settings.go
@@ -82,6 +82,20 @@ func (a *APISettingsAPIService) ClientApiSettingsV1PostExecute(r ApiClientApiSet
 	if localVarHTTPHeaderAccept != "" {
 		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
 	}
+	if r.ctx != nil {
+		// API Key Authentication
+		if auth, ok := r.ctx.Value(ContextAPIKeys).(map[string]APIKey); ok {
+			if apiKey, ok := auth["BearerAuth"]; ok {
+				var key string
+				if apiKey.Prefix != "" {
+					key = apiKey.Prefix + " " + apiKey.Key
+				} else {
+					key = apiKey.Key
+				}
+				localVarHeaderParams["Authorization"] = key
+			}
+		}
+	}
 	req, err := a.client.prepareRequest(r.ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, formFiles)
 	if err != nil {
 		return localVarReturnValue, nil, err
